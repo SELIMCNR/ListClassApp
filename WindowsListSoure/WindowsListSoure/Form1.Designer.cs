05f4b487ec7e999d773e14f00c83618a17843e78
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2024-11-04 18:43:23 +0300

    Add files via upload

---
+++ WindowsListSoure/WindowsListSoure/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsListSoure
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnlist1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnlist2 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.btnlist3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(175, 290);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnlist1
+            // 
+            this.btnlist1.Location = new System.Drawing.Point(13, 313);
+            this.btnlist1.Name = "btnlist1";
+            this.btnlist1.Size = new System.Drawing.Size(175, 30);
+            this.btnlist1.TabIndex = 1;
+            this.btnlist1.Text = "Listele 1";
+            this.btnlist1.UseVisualStyleBackColor = true;
+            this.btnlist1.Click += new System.EventHandler(this.btnlist1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(229, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(176, 290);
+            this.listBox2.TabIndex = 0;
+            // 
+            // btnlist2
+            // 
+            this.btnlist2.Location = new System.Drawing.Point(229, 312);
+            this.btnlist2.Name = "btnlist2";
+            this.btnlist2.Size = new System.Drawing.Size(176, 30);
+            this.btnlist2.TabIndex = 1;
+            this.btnlist2.Text = "Listele 2";
+            this.btnlist2.UseVisualStyleBackColor = true;
+            this.btnlist2.Click += new System.EventHandler(this.btnlist2_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(439, 12);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(166, 290);
+            this.listBox3.TabIndex = 0;
+            // 
+            // btnlist3
+            // 
+            this.btnlist3.Location = new System.Drawing.Point(439, 312);
+            this.btnlist3.Name = "btnlist3";
+            this.btnlist3.Size = new System.Drawing.Size(166, 30);
+            this.btnlist3.TabIndex = 1;
+            this.btnlist3.Text = "Listele 3";
+            this.btnlist3.UseVisualStyleBackColor = true;
+            this.btnlist3.Click += new System.EventHandler(this.btnlist3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 355);
+            this.Controls.Add(this.btnlist3);
+            this.Controls.Add(this.btnlist2);
+            this.Controls.Add(this.btnlist1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnlist1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnlist2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button btnlist3;
+    }
+}
+
---
+++ WindowsListSoure/WindowsListSoure/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsListSoure
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlist1_Click(object sender, EventArgs e)
+        {
+            List<string> kisiler = new List<string>();
+            kisiler.Add("Mazhar");
+            kisiler.Add("Ruhsar");
+            kisiler.Add("Menkıb");
+            kisiler.Add("Müfit");
+            kisiler.Add("Reyhan");
+            kisiler.Add("Firdevs");
+
+            kisiler.Remove("Müfit");
+
+            foreach (string s in kisiler)
+            {
+                listBox1.Items.Add(s);
+
+            }
+        }
+
+        private void btnlist2_Click(object sender, EventArgs e)
+        {
+            List <int> sayilar = new List<int>();
+            sayilar.Add(45);
+            sayilar.Add(74);
+            sayilar.Add(25);
+            sayilar.Add(33);
+            sayilar.Add(22);
+            sayilar.Add(15);
+            sayilar.Add(14);
+
+
+            foreach (int i in sayilar)
+            {
+
+                if (i % 5 == 0)
+                {
+                    listBox2.Items.Add(i);
+                }
+
+            }
+
+            if (sayilar.Contains(74))
+            {
+                MessageBox.Show("Bu sayı var.");
+            }
+            else
+            {
+                MessageBox.Show("Bu sayı yok.");
+            }
+
+        }
+
+        private void btnlist3_Click(object sender, EventArgs e)
+        {
+            List<Kisiler> kisi = new List<Kisiler>();
+            kisi.Add(new Kisiler()
+            {
+
+                ADI ="Yusuf",
+                SOYADI ="Çınar",
+                MESLEKI = "Uzay Mühendisi"
+            });
+
+            foreach (Kisiler k in kisi )
+            {
+                listBox3.Items.Add(k.ADI + " " + k.SOYADI + " " + k.MESLEKI);
+            }
+        }
+    }
+}
